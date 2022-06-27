da03da1a8442ab508b4131724b0715ad0fea5113
Nick Embrey <cne.embrey@gmail.com>
2022-06-26 21:32:42 -0700

    add test directory

---
+++ src/test/kotlin/engine/GameOperationsKtTest.kt
@@ -0,0 +1,52 @@
+package engine
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class GameOperationsKtTest {
+
+    @Test
+    fun playActionCard() {
+    }
+
+    @Test
+    fun playTreasureCard() {
+    }
+
+    @Test
+    fun drawCard() {
+    }
+
+    @Test
+    fun shuffle() {
+    }
+
+    @Test
+    fun buyCard() {
+    }
+
+    @Test
+    fun decideGainCard() {
+    }
+
+    @Test
+    fun gainCard() {
+    }
+
+    @Test
+    fun trashCards() {
+    }
+
+    @Test
+    fun trashCard() {
+    }
+
+    @Test
+    fun discardCards() {
+    }
+
+    @Test
+    fun discardCard() {
+    }
+}
---
+++ src/test/kotlin/engine/GameStateTest.kt
@@ -0,0 +1,38 @@
+package engine
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class GameStateTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun initialize() {
+    }
+
+    @Test
+    fun nextPhase() {
+    }
+
+    @Test
+    fun getNextChoices() {
+    }
+
+    @Test
+    fun makeNextDecision() {
+    }
+
+    @Test
+    fun applyDecision() {
+    }
+}
