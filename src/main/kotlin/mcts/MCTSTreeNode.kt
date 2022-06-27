84af665be5a73a8ede37957084b389f4f0ca0530
Nick Embrey <cne.embrey@gmail.com>
2022-06-26 18:01:26 -0700

    expand MCTSTreeNode to provide better debug

--- src/main/kotlin/mcts/MCTSTreeNode.kt
+++ src/main/kotlin/mcts/MCTSTreeNode.kt
@@ -1,10 +1,18 @@
 package mcts
 
+import engine.CardChoices
+import engine.ChoiceContext
 import engine.PlayerNumber
 
 // TODO: reconsider how we're using wins vs score and optional PolicyPlayer
 //  this might be a good place to use a subclass
-class MCTSTreeNode(val parent: MCTSTreeNode? = null, val decision: Int? = null, val player: PlayerNumber? = null) {
+class MCTSTreeNode( // TODO make a debug version that takes the state and gets all of this stuff
+    val parent: MCTSTreeNode? = null,
+    val decision: Int? = null,
+    val player: PlayerNumber? = null,
+    val choice: Any? = null,
+    val context: ChoiceContext? = null
+) {
     // TODO: maybe we should get rid of one of these or do something clever to unify them
     var wins: Int = 0
     var score: Double = 0.0
@@ -12,7 +20,12 @@ class MCTSTreeNode(val parent: MCTSTreeNode? = null, val decision: Int? = null, 
 
     var children: MutableList<MCTSTreeNode> = mutableListOf()
 
-    fun addChild(decision: Int, player: PlayerNumber? = null) {
-        children.add(MCTSTreeNode(this, decision, player))
+    fun addChild(
+        decision: Int,
+        player: PlayerNumber? = null,
+        choice: Any? = null,
+        context: ChoiceContext? = null
+    ) {
+        children.add(MCTSTreeNode(this, decision, player, choice, context))
     }
 }
