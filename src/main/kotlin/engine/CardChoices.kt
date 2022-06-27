2f612c38f3ea650bbabd3329079dc0e5eeee4765
Nick Embrey <cne.embrey@gmail.com>
2022-06-26 17:53:35 -0700

    move getCardChoices to CardChoices.kt

--- src/main/kotlin/engine/CardChoices.kt
+++ src/main/kotlin/engine/CardChoices.kt
@@ -1,5 +1,7 @@
 package engine
 
+import util.combinations
+
 interface CardChoices { // TODO: add getting card function here
     val choices: List<*> // TODO: I wonder if this could be any better...
     fun isEmpty(): Boolean = choices.isEmpty()
@@ -15,3 +17,38 @@ value class SingleCardChoices(override val choices: List<Card?> = listOf()): Car
 value class MultipleCardChoices(override val choices: List<List<Card>> = listOf()): CardChoices
 // TODO: consider changing to a set
 // TODO: make sure you can always choose no card or a list of empty cards
+
+// TODO: do we need player?
+fun getCardChoices(state: GameState, player: Player, context: ChoiceContext): CardChoices {
+    return when(context) {
+        ChoiceContext.ACTION -> {
+            if(player.actions > 0) {
+                val actionCards = player.hand.filter { it.type == CardType.ACTION }.distinct()
+                if(actionCards.isNotEmpty()) {
+                    return SingleCardChoices(actionCards + listOf(null))
+                }
+            }
+            return SingleCardChoices()
+        }
+        ChoiceContext.TREASURE -> SingleCardChoices(player.hand.filter { it.type == CardType.TREASURE }.distinct() + listOf(null))
+        ChoiceContext.BUY -> {
+            if(player.buys > 0) {
+                SingleCardChoices(state.board.filter { it.value > 0 && player.coins >= it.key.cost }.keys.toList() + listOf(null))
+            } else {
+                SingleCardChoices()
+            }
+        }
+        ChoiceContext.MILITIA -> {
+            if(player.hand.size > 3) {
+                MultipleCardChoices(player.hand.combinations(player.hand.size - 3).toList())
+            } else {
+                MultipleCardChoices()
+            }
+        }
+        ChoiceContext.WORKSHOP -> SingleCardChoices(state.board.filter { it.key.cost < 5 && it.value > 0 }.keys.toList())
+        ChoiceContext.CHAPEL -> MultipleCardChoices(( player.hand.combinations(4) +
+                player.hand.combinations(3) +
+                player.hand.combinations(2) +
+                player.hand.combinations(1)).toList())
+    }
+}
--- src/main/kotlin/engine/ChoiceContext.kt
+++ src/main/kotlin/engine/ChoiceContext.kt
@@ -5,27 +5,4 @@ import util.combinations
 enum class ChoiceContext {
     ACTION, TREASURE, BUY, CHAPEL, MILITIA, WORKSHOP;
 
-    fun getCardChoices(state: GameState, player: Player): CardChoices {
-        return when(this) {
-            ACTION -> if(player.actions > 0) {
-                SingleCardChoices(player.hand.filter { it.type == CardType.ACTION })
-            } else {
-                SingleCardChoices()
-            }
-            TREASURE -> SingleCardChoices(player.hand.filter { it.type == CardType.TREASURE })
-            BUY -> SingleCardChoices(state.board.filter { it.value > 0 && player.coins >= it.key.cost }.keys.toList())
-            MILITIA -> {
-                if(player.hand.size > 3) {
-                    MultipleCardChoices(player.hand.combinations(player.hand.size - 3).toList())
-                } else {
-                    MultipleCardChoices()
-                }
-            }
-            WORKSHOP -> SingleCardChoices(state.board.filter { it.key.cost < 5 && it.value > 0 }.keys.toList())
-            CHAPEL -> MultipleCardChoices(( player.hand.combinations(4) +
-                    player.hand.combinations(3) +
-                    player.hand.combinations(2) +
-                    player.hand.combinations(1)).toList())
-        }
-    }
 }
