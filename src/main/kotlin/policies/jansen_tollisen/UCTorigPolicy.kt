da5e620c8ff540cbe475541f452665490d55fccd
Nick Embrey <cne.embrey@gmail.com>
2022-07-01 15:46:25 -0700

    UCTorigPolicy improvements
    - shuffle before starting simulation so the player isn't given knowledge of the ordering of their deck (this was a BIG bug conceptually but doesn't seem to affect performance that much interestingly enough)
    
    - use the epsilonHeuristicGreedyPolicy as the playout policy
    
    - max turns of 40 for a playout
    
    - only include children when there are multiple choices
    
    - simulate by number of iterations, not by time

--- src/main/kotlin/policies/jansen_tollisen/UCTorigPolicy.kt
+++ src/main/kotlin/policies/jansen_tollisen/UCTorigPolicy.kt
@@ -3,7 +3,7 @@ package policies.jansen_tollisen
 import engine.*
 import engine.Player
 import mcts.MCTSTreeNode
-import policies.policy.randomPolicy
+import policies.policy.epsilonHeuristicGreedyPolicy
 import kotlin.math.ln
 
 fun _UCTorigPolicy(
@@ -11,10 +11,10 @@ fun _UCTorigPolicy(
     choices: CardChoices
 ): Card? {
 
-    // play action cards first (MPPAF)
+    // must play plus actions first (MPPAF)
     if(state.context == ChoiceContext.ACTION) {
         val addActionsCard = choices.filterNotNull().firstOrNull { it.addActions > 0 }   // TODO:
-        if (addActionsCard != null) { // TODO: will it ever be null?
+        if (addActionsCard != null) {
             return addActionsCard
         }
     } else if(state.context == ChoiceContext.TREASURE) {
@@ -22,8 +22,9 @@ fun _UCTorigPolicy(
     }
 
     // TODO: pull out into settings either at Policy or Simulation level or both
-    val playerNumber = PlayerNumber.PlayerTwo
-    val seconds = 1
+
+    // TODO: need a settings class
+    val seconds = 2.0
     val cParameter = 0.7
 
 //    state.logger?.recordSimulationOptions(seconds, cParameter) // TODO
@@ -38,39 +39,43 @@ fun _UCTorigPolicy(
     // TODO: remove subfunctions from here and in MCTS policy
     // Note that toMutableList is used below to create copies of the current state.
     // TODO: this could be a bottleneck? think about it.
-    fun getNewState(currentState: GameState): GameState {
+    fun getNewState(currentState: GameState, shuffle: Boolean = false): GameState {
         val playerOne = Player(
             "Player One",
             PlayerNumber.PlayerOne,
-            ::randomPolicy,
-            currentState.playerOne.deck.toMutableList(),
+            epsilonHeuristicGreedyPolicy,
+            currentState.playerOne.deck.toMutableList().also { if(shuffle) it.shuffle() }, // TODO:
             currentState.playerOne.hand.toMutableList(),
             currentState.playerOne.inPlay.toMutableList(),
             currentState.playerOne.discard.toMutableList()
-        )
+        ).apply { remodelCard = currentState.playerOne.remodelCard }
         val playerTwo = Player(
             "Player Two",
             PlayerNumber.PlayerTwo,
-            ::randomPolicy,
-            currentState.playerTwo.deck.toMutableList(),
+            epsilonHeuristicGreedyPolicy,
+            currentState.playerTwo.deck.toMutableList().also { if(shuffle) it.shuffle() }, // TODO:
             currentState.playerTwo.hand.toMutableList(),
             currentState.playerTwo.inPlay.toMutableList(),
             currentState.playerTwo.discard.toMutableList()
-        )
+        ).apply { remodelCard = currentState.playerOne.remodelCard }
         return GameState(
             playerOne,
             playerTwo,
             currentState.board.toMutableMap(),
             currentState.turns,
             currentState.context,
-            trueShuffle = false
-        ).apply { currentPlayer = currentState.currentPlayer }
+            trueShuffle = false,
+            maxTurns = 40
+        ).apply { currentPlayer = when(currentState.currentPlayer.playerNumber) {
+            PlayerNumber.PlayerOne -> playerOne
+            PlayerNumber.PlayerTwo -> playerTwo
+        } }
     }
 
     fun rollout(simState: GameState): Map<PlayerNumber, Double> {
 
         while(!simState.gameOver) {
-            simState.choicePlayer.makeNextCardDecision(simState, ::randomPolicy) // TODO: is this the right policy?
+            simState.choicePlayer.makeNextCardDecision(simState, epsilonHeuristicGreedyPolicy)
         }
 
         val playerOneVp = simState.playerOne.vp
@@ -91,22 +96,43 @@ fun _UCTorigPolicy(
 
     fun forward(node: MCTSTreeNode, simState: GameState, simChoices: CardChoices) {
 
-        if(node.children.size > 0) {
-            val simDecisionIndex = if(node.children.any { it.simulations == 0 }) {
-                node.children.indexOf(node.children.first { it.simulations == 0 })
-            } else {
-                val menu: List<Double> = node.children.map {
-                    (it.score / it.simulations) +
-                            (cParameter * kotlin.math.sqrt(ln(node.simulations.toDouble()) / it.simulations))
+        if(node.children.isNotEmpty()) {
+            val index =
+                if (node.children.size == 1) {
+                    0 // TODO: this should never happen, right?
+                } else if (node.children.any { it.simulations == 0 }) { // i.e., each child has not had a simulation
+                    node.children.indexOfFirst { it.simulations == 0 }
+                } else {
+                    node.children.map {
+                        (it.score / it.simulations) +
+                                (cParameter * kotlin.math.sqrt(ln(node.simulations.toDouble()) / it.simulations))
+                    }.let { values ->
+                        values.indices.maxByOrNull { values[it] }!!
+                    }
                 }
-                menu.indexOf(menu.maxOf { it })
+
+            simChoices[index].let {
+                simState.choicePlayer.makeCardDecision(it, simState)
             }
 
-            val card = simChoices[simDecisionIndex]!!
+            // TODO: wrap this up in a method
+            var nextChoices = simState.context.getCardChoices(simState.choicePlayer, simState.board)
+            while(true) {
+                if (nextChoices.size == 1) {
+                    simState.choicePlayer.makeCardDecision(nextChoices[0], simState)
+                } else when (simState.context) {
+                    ChoiceContext.ACTION -> { // must play plus actions first (MPPAF)
+                        nextChoices.filterNotNull().firstOrNull { it.addActions > 0 }?.let {
+                            simState.choicePlayer.makeCardDecision(it, simState)
+                        } ?: break
+                    }
+                    ChoiceContext.TREASURE -> simState.choicePlayer.makeCardDecision(nextChoices[0], simState)
+                    else -> break
+                }
+                nextChoices = simState.context.getCardChoices(simState.choicePlayer, simState.board)
+            }
 
-            simState.choicePlayer.makeCardDecision(card, simState)
-
-            forward(node.children[simDecisionIndex], simState, simState.context.getCardChoices(simState.choicePlayer, simState.board))
+            forward(node.children[index], simState, nextChoices)
         } else {
 
             node.simulations = 1
@@ -124,7 +150,7 @@ fun _UCTorigPolicy(
             // backpropagation
             var current = node.parent
             while(current != null) {
-                current.score += rolloutResults[current.player]!!
+                current.score += rolloutResults[current.playerNumber]!!
                 current.simulations += 1
                 current = current.parent
             }
@@ -132,16 +158,16 @@ fun _UCTorigPolicy(
     }
 
 
-    val end = System.currentTimeMillis() + seconds * 1000
-    while (System.currentTimeMillis() < end) {
+    val iterations = 100000
+    val shuffledState = getNewState(state, true)
+    for (it in 1..iterations) {
         state.logger?.addPlayout()
-        forward(root, getNewState(state), choices)
+        forward(root, getNewState(shuffledState), choices)
     }
 
     state.logger?.addDecision()
 
     val simulations: List<Int> = root.children.map { it.simulations }
-    val maxSim = simulations.maxOf { it }
-
-    return choices[maxSim]
+    val index = simulations.indices.maxByOrNull { simulations[it] }!!
+    return choices[index]
 }
