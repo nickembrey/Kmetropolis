f73d52e7e1d074a942710a49bdc9c2ad86e392e5
Nick Embrey <cne.embrey@gmail.com>
2022-07-01 15:29:01 -0700

    fix sorting bugs in heuristicGreedyPolicy

--- src/main/kotlin/policies/rollout/jansen_tollisen/HeuristicGreedyPolicy.kt
+++ src/main/kotlin/policies/rollout/jansen_tollisen/HeuristicGreedyPolicy.kt
@@ -2,25 +2,25 @@ package policies.rollout.jansen_tollisen
 
 import engine.*
 import engine.Player
+import policies.policy.randomPolicy
 
 fun _heuristicGreedyPolicy(
     state: GameState,
     choices: CardChoices
 ): Card? {
     return when(state.context) {
-        ChoiceContext.ACTION -> { // TODO: unchecked cast
-
-            // order by whether it has actions first, then by cost (MPPAF)
-            // TODO: quick test
-            choices.sortedWith(compareBy( { it?.addActions }, { it?.cost }))[0] // TODO: unchecked cast
+        ChoiceContext.ACTION -> {
+            // order by number of actions first, then by cost
+            choices.sortedWith(compareBy ( { it?.addActions }, { it?.cost }) ).reversed()[0]
         }
-        ChoiceContext.TREASURE -> choices[0] // TODO: this is wrong, see MCTS policy
-        ChoiceContext.BUY -> { // TODO: unchecked cast
+        ChoiceContext.TREASURE -> choices[0]
+        ChoiceContext.BUY -> {
             // TODO: weird corner case where all coppers and all curses are gone?
-            choices.filter { it != Card.CURSE }.sortedWith(compareBy { it?.cost })[0] // TODO: make sure null will never be first?
+            choices.filter { it != Card.CURSE }.sortedWith(compareByDescending { it?.cost })[0] // TODO: make sure null will never be first?
         }
         ChoiceContext.MILITIA -> throw NotImplementedError()
         ChoiceContext.WORKSHOP -> throw NotImplementedError()
         ChoiceContext.CHAPEL -> throw NotImplementedError()
+        else -> randomPolicy(state, choices)
     }
 }
